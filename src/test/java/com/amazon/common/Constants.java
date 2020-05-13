43b1daa7e7f76534ca11878b769d6d7da71bf981
karthikakarthick <karthickit18@gmail.com>
2020-05-13 19:15:06 +0530

    Final version

--- src/test/java/com/amazon/common/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.amazon.common;
-
-public class Constants {
-	 public static final String PROPERTY_FILE_PATH = "src/main/resources/configs/locators.properties";
-	 public static final String APP_PROPERTIES_FILE_PATH ="src\\main\\resources\\configs\\common.properties";
-}
-
-
